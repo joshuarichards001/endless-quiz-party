6bee826f903b13a5d219d439651c29d4edbae6c7
Josh Richards <joshuarichards001@gmail.com>
2025-06-29 17:56:27 +0100

    Enhance IP extraction logging in getIPFromRequest function

--- server/utils.go
+++ server/utils.go
@@ -17,12 +17,15 @@ func loadEnv() {
 
 func getIPFromRequest(r *http.Request) string {
 	if ip := r.Header.Get("X-Real-Ip"); ip != "" {
+		log.Println("Utils - IP extracted from X-Real-Ip header:", ip)
 		return ip
 	}
 
 	if ip := r.Header.Get("X-Forwarded-For"); ip != "" {
+		log.Println("Utils - IP extracted from X-Forwarded-For header:", ip)
 		return strings.Split(ip, ",")[0]
 	}
 
+	log.Println("Utils - IP extracted from RemoteAddr:", r.RemoteAddr)
 	return r.RemoteAddr
 }
